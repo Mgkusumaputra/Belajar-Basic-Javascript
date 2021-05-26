e02c70e41043b0a0a1908774e80ecaaca16405ee
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2021-05-26 12:34:21 +0700

    Membuat Tipe Data Number .Js

---
+++ Tipe Data Number/Tipe Data Number.js
@@ -0,0 +1,20 @@
+        // Basis 10
+        document.writeln("Basis 10 </br>");
+        document.writeln(100);
+        document.writeln("</br>");
+        document.writeln(100.100);
+
+        // Hexadecimal
+        document.writeln("</br> </br>");
+        document.writeln("Hexadecimal </br>");
+        document.writeln(0xC8);
+
+        // Binary
+        document.writeln("</br> </br>");
+        document.writeln("Binary </br>");
+        document.writeln(0b100101100);
+
+        // Octal
+        document.writeln("</br> </br>");
+        document.writeln("Octal </br>");
+        document.writeln(0o620);
