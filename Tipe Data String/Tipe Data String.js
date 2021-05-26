bfd9897f3174abcecb03812370ad794611abff77
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2021-05-26 17:06:07 +0700

    Mengisi Script Di File .js

---
+++ Tipe Data String/Tipe Data String.js
@@ -0,0 +1,34 @@
+// "
+document.writeln("Mgkusumaputra");
+document.writeln("</br>");
+
+// '
+document.writeln("MGKPs");
+document.writeln("</br> </br>");
+
+// Menambahkan String
+document.writeln("Angka -" + " Angka");
+document.writeln("</br> </br>");
+
+// Escape Sequence
+document.writeln("<textarea cols='100' rows='10'>");
+// \n - Enter
+document.writeln("Enter");
+document.writeln("123 \n456 \n789");
+document.writeln("\n");
+// \t - Tab
+document.writeln("Tab");
+document.writeln("100 \t200 \t300");
+document.writeln("\n");
+// \' - '
+document.writeln("Petik Satu (')");
+document.writeln("'121'");
+document.writeln("\n");
+// \" - "
+document.writeln("Petik Dua");
+document.writeln('"120"');
+document.writeln("\n");
+// \\ - \
+document.writeln("(/)");
+document.writeln("Javascript \\ Typescript");
+document.writeln("</textarea>");
