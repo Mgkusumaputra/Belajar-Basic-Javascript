7553706b3e50811815e4dc6c58d0a3ff3fdc847f
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2021-05-27 19:21:56 +0700

    Mengisi Script FIle .js

--- Operator Perbandingan/Operator Perbandingan.js
+++ Operator Perbandingan/Operator Perbandingan.js
@@ -1,1 +1,33 @@
 document.writeln("</br> </br> Javascript </br>");
+
+// Sama Dengan
+let hasil = 5 == "5";
+document.writeln("<p>" + hasil + "</p>");
+
+// Sama Dengan Sama Tipe
+hasil = 5 === "5";
+document.writeln("<p>" + hasil + "</p>");
+
+// Lebih Dari
+hasil = 5 > "10";
+document.writeln("<p>" + hasil + "</p>");
+
+// Kurang Dari
+hasil = 5 < "10";
+document.writeln("<p>" + hasil + "</p>");
+
+// Lebih Dari Sama Dengan
+hasil = 5 >= "10";
+document.writeln("<p>" + hasil + "</p>");
+
+// Kurang Dari Sama Dengan
+hasil = 5 <= "10";
+document.writeln("<p>" + hasil + "</p>");
+
+// Tidak Sama Dengan
+hasil = 5 != "10";
+document.writeln("<p>" + hasil + "</p>");
+
+// Tidak Sama Dengan / Tidak Sama Tipe
+hasil = 5 !== "10";
+document.writeln("<p>" + hasil + "</p>");
